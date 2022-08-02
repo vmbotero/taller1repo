f13960756c3c60ae6c564fdc5ccd0138f3b89fef
vmbotero <boterogomezvictor@gmail.com>
2022-08-02 10:20:39 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'home.html', {'name':'Greg Lim'})
+    return render(request,'home.html', {'name':'Victor Manuel Botero The Best'})
     #return HttpResponse('<h1>Welcome To Home Page</h1>')
