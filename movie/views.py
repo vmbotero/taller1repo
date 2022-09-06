7b8d58d6d3257921a39a59cb18ec0161c8eae709
vmbotero <boterogomezvictor@gmail.com>
2022-09-06 12:28:32 -0500

    Visualizacion de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
-    return render(request,'home.html', {'name':'Victor Manuel Botero The Best'})
+    #return render(request,'home.html', {'name':'Victor Manuel Botero The Best'})
     #return HttpResponse('<h1>Welcome To Home Page</h1>')
+    searchterm = request.GET.get('searchMovie')
+    if searchterm:
+        movies = Movie.objects.filter(title__icontains=searchterm)
+    else:
+        movies = Movie.objects.all()
+    return render(request,'home.html', {'searchterm': searchterm, 'movies': movies})
     
 def about(request):
     return render(request,'about.html')
--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -13,7 +13,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+import os
+BASE_DIR= Path(__file__).resolve().parent.parent
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
