71f9039310f5b814ecfcb2d9b5f67164dcd8ae81
vmbotero <boterogomezvictor@gmail.com>
2022-09-06 12:46:21 -0500

    Formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Movie
+#from .models import Movie
 
 def home(request):
     #return render(request,'home.html', {'name':'Victor Manuel Botero The Best'})
     #return HttpResponse('<h1>Welcome To Home Page</h1>')
     searchterm = request.GET.get('searchMovie')
-    if searchterm:
-        movies = Movie.objects.filter(title__icontains=searchterm)
-    else:
-        movies = Movie.objects.all()
-    return render(request,'home.html', {'searchterm': searchterm, 'movies': movies})
+    #if searchterm:
+    #    movies = Movie.objects.filter(title__icontains=searchterm)
+    #else:
+    #    movies = Movie.objects.all()
+    #return render(request,'home.html', {'searchterm': searchterm, 'movies': movies})
+    return render(request,'home.html', {'searchterm': searchterm})
     
 def about(request):
     return render(request,'about.html')
